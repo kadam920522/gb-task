79f818824e66a563500bb4aa5370b7a003aa7c4c
Adam <k.adam920522@yahoo.com>
2021-12-10 13:50:51 +0100

    Moved InternalsVisibleTo into csproj

--- GlobalBlue.CustomerManager/src/Application/DependencyInjection.cs
+++ GlobalBlue.CustomerManager/src/Application/DependencyInjection.cs
@@ -5,9 +5,6 @@ using GlobalBlue.CustomerManager.Application.Common.Concrete;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("GlobalBlue.CustomerManager.Application.UnitTests")]
 
 namespace GlobalBlue.CustomerManager.Application
 {
