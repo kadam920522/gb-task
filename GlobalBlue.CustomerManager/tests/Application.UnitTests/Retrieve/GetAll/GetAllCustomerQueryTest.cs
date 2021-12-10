f0f8c15440e25a7d81b11281fb507d0192dfa766
Adam <k.adam920522@yahoo.com>
2021-12-10 17:16:43 +0100

    Added unit tests for retrieve operations

---
+++ GlobalBlue.CustomerManager/tests/Application.UnitTests/Retrieve/GetAll/GetAllCustomerQueryTest.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using GlobalBlue.CustomerManager.Application.Entities;
+using GlobalBlue.CustomerManager.Application.Retrieve.GetAll;
+using Moq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace GlobalBlue.CustomerManager.Application.UnitTests.Retrieve.GetAll
+{
+    public class GetAllCustomerQueryTest : ApplicationTestBase
+    {
+        [Fact]
+        public async Task ShouldReturnAllTheCustomers()
+        {
+            // Arrange
+            var expectedCustomers = new[]
+            {
+                new Customer(), new Customer(), new Customer()
+            };
+
+            _customerStorageMock.Setup(storage => storage.GetAllAsync()).ReturnsAsync(expectedCustomers);
+
+            // Act
+            var customers = await _sender.Send(new GetAllCustomerQuery());
+
+            // Assert
+            customers.Should().BeEquivalentTo(expectedCustomers);
+        }
+    }
+}
---
+++ GlobalBlue.CustomerManager/tests/Application.UnitTests/Retrieve/GetById/GetCustomerByIdQueryTest.cs
@@ -0,0 +1,46 @@
+﻿using FluentAssertions;
+using GlobalBlue.CustomerManager.Application.Entities;
+using GlobalBlue.CustomerManager.Application.Exceptions;
+using GlobalBlue.CustomerManager.Application.Retrieve.GetById;
+using Moq;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace GlobalBlue.CustomerManager.Application.UnitTests.Retrieve.GetById
+{
+    public class GetCustomerByIdQueryTest : ApplicationTestBase
+    {
+        [Fact]
+        public void ShouldThrowCustomerNotFoundException_WhenNoSuchCustomerWithTheSpecifiedId()
+        {
+            // Arrange
+            const int NOT_EXISTING_CUSTOMER_ID = 5;
+            var query = new GetCustomerByIdQuery(NOT_EXISTING_CUSTOMER_ID);
+
+            _customerStorageMock.Setup(storage => storage.GetByIdAsync(NOT_EXISTING_CUSTOMER_ID)).ReturnsAsync((Customer)null);
+
+            // Act
+            Func<Task<Customer>> act = () => _sender.Send(query);
+
+            // Assert
+            act.Should().ThrowAsync<CustomerNotFoundException>().WithMessage($"Customer with ID:{NOT_EXISTING_CUSTOMER_ID} doesn't exist");
+        }
+
+        [Fact]
+        public async Task ShouldReturnCustomerWithTheSpecifiedId()
+        {
+            // Arrange
+            const int CUSTOMER_ID = 5;
+            var query = new GetCustomerByIdQuery(CUSTOMER_ID);
+
+            _customerStorageMock.Setup(storage => storage.GetByIdAsync(CUSTOMER_ID)).ReturnsAsync(new Customer());
+
+            // Act
+            var customer = await _sender.Send(query);
+
+            // Assert
+            customer.Should().NotBeNull();
+        }
+    }
+}
